4ebb3f57dce7e31c914ee7759b142a7444faf70b
Михаил Дюсов <143973@gmail.com>
2026-02-11 17:29:05 +0300

    feature(habit-agenda): add loading habits (with test data)

--- feature/habit-agenda/impl/src/main/java/com/dyusov/feature/habit/agenda/impl/HabitAgendaViewModel.kt
+++ feature/habit-agenda/impl/src/main/java/com/dyusov/feature/habit/agenda/impl/HabitAgendaViewModel.kt
@@ -1,22 +1,109 @@
 package com.dyusov.feature.habit.agenda.impl
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dyusov.core.common.datetime.nowClock
+import com.dyusov.core.common.utils.onError
+import com.dyusov.core.common.utils.onSuccess
+import com.dyusov.core.domain.habit.GetAllHabitsUseCase
+import com.dyusov.core.domain.habit.UpdateHabitUseCase
 import com.dyusov.core.model.Habit
+import com.dyusov.core.model.HabitFrequency
 import dagger.hilt.android.lifecycle.HiltViewModel
-import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
+import javax.inject.Inject
 
 @HiltViewModel
 class HabitAgendaViewModel @Inject constructor(
-    // TODO: add use-cases
+    private val getAllHabitsUseCase: GetAllHabitsUseCase,
+    // TODO: for test
+    private val updateHabitUseCase: UpdateHabitUseCase
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(HabitAgendaState())
     val state: StateFlow<HabitAgendaState> = _state.asStateFlow()
+
+    init {
+        createTestHabits()
+        loadHabits()
+    }
+
+    private fun loadHabits() {
+        getAllHabitsUseCase()
+            .onEach { result ->
+                result
+                    .onSuccess { habitList ->
+                        Log.d(
+                            "HabitAgendaViewModel",
+                            "Got habits: $habitList"
+                        )
+                        _state.update { currentState ->
+                            currentState.copy(items = habitList)
+                        }
+                    }
+                    .onError { error ->
+                        Log.d(
+                            "HabitAgendaViewModel",
+                            "Got error while getting habits: $error"
+                        )
+                        _state.update {
+                            it.copy(error = error.toString())
+                        }
+                    }
+            }.launchIn(viewModelScope)
+    }
+
+    // TODO: for test
+    private fun createTestHabits() {
+        viewModelScope.launch {
+            updateHabitUseCase(
+                Habit(
+                    id = 1,
+                    isCompletedToday = false,
+                    createdAt = System.currentTimeMillis(),
+                    updatedAt = System.currentTimeMillis(),
+                    name = "Зарядка",
+                    description = "Утренняя зарядка 10 минут",
+                    frequency = HabitFrequency.Daily,
+                    color = 0xFF4CAF50.toInt()
+                )
+
+            )
+            updateHabitUseCase(
+                Habit(
+                    id = 2,
+                    isCompletedToday = false,
+                    createdAt = System.currentTimeMillis(),
+                    updatedAt = System.currentTimeMillis(),
+                    name = "Чтение",
+                    description = "Читать 30 минут перед сном",
+                    frequency = HabitFrequency.Daily,
+                    color = 0xFF2196F3.toInt()
+                )
+            )
+            updateHabitUseCase(
+                Habit(
+                    id = 3,
+                    isCompletedToday = false,
+                    createdAt = System.currentTimeMillis(),
+                    updatedAt = System.currentTimeMillis(),
+                    name = "Медитация",
+                    description = "5 минут медитации",
+                    frequency = HabitFrequency.Daily,
+                    color = 0xFF9C27B0.toInt()
+                )
+            )
+        }
+
+    }
 }
 
 // commands
