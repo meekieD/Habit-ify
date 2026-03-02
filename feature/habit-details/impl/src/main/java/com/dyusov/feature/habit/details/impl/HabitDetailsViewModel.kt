86b0a94d13b7f18441dbc3538cb13d2fa9720397
Mihail Dyusov <143973@gmail.com>
2026-03-02 21:17:05 +0300

    feature(habit-details): update habit details view model

--- feature/habit-details/impl/src/main/java/com/dyusov/feature/habit/details/impl/HabitDetailsViewModel.kt
+++ feature/habit-details/impl/src/main/java/com/dyusov/feature/habit/details/impl/HabitDetailsViewModel.kt
@@ -1,17 +1,30 @@
 package com.dyusov.feature.habit.details.impl
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dyusov.core.common.datetime.now
+import com.dyusov.core.common.datetime.toEndOfMonthTimestamp
+import com.dyusov.core.common.datetime.toStartOfMonthTimestamp
+import com.dyusov.core.common.utils.onError
+import com.dyusov.core.common.utils.onSuccess
 import com.dyusov.core.domain.tracking.GetHabitWithCompletionsInPeriodUseCase
-import com.dyusov.core.domain.tracking.GetHabitWithCompletionsUseCase
 import com.dyusov.core.domain.tracking.ToggleHabitCompletionOnDateUseCase
+import com.dyusov.core.model.Habit
+import com.dyusov.core.model.HabitCompletion
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.YearMonth
 
 @HiltViewModel(assistedFactory = HabitDetailsViewModel.Factory::class)
 class HabitDetailsViewModel @AssistedInject constructor(
-    private val getHabitWithCompletionsUseCase: GetHabitWithCompletionsUseCase,
     private val getHabitWithCompletionsInPeriodUseCase: GetHabitWithCompletionsInPeriodUseCase,
     private val toggleHabitCompletionOnDateUseCase: ToggleHabitCompletionOnDateUseCase,
     @Assisted("habitId") private val habitId: Long
@@ -21,4 +34,89 @@ class HabitDetailsViewModel @AssistedInject constructor(
     interface Factory {
         fun create(@Assisted("habitId") habitId: Long): HabitDetailsViewModel
     }
+
+    private val _state = MutableStateFlow<HabitDetailsState>(
+        HabitDetailsState.Initial
+    )
+
+    val state = _state.asStateFlow()
+
+    init {
+        loadHabitDetails(YearMonth.now())
+    }
+
+    fun processCommand(command: HabitDetailsCommand) {
+        viewModelScope.launch {
+            when (command) {
+                is HabitDetailsCommand.ToggleHabitCompletionOnDate -> {
+                    toggleHabitCompletionOnDateUseCase(
+                        habitId = habitId,
+                        date = command.selectedDate
+                    )
+                    loadHabitDetails(
+                        month = (_state.value as? HabitDetailsState.Content)?.currentMonth
+                            ?: YearMonth.now()
+                    )
+                }
+
+                is HabitDetailsCommand.SetDisplayedMonth -> {
+                    loadHabitDetails(command.selectedMonth)
+                }
+
+                is HabitDetailsCommand.Back -> {
+                    _state.update {
+                        HabitDetailsState.Finished
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadHabitDetails(month: YearMonth) {
+        viewModelScope.launch {
+            getHabitWithCompletionsInPeriodUseCase(
+                habitId = habitId,
+                startTimestamp = month.toStartOfMonthTimestamp(),
+                endTimestamp = month.toEndOfMonthTimestamp()
+            ).onSuccess { habitWithCompletions ->
+                Log.d(
+                    "HabitDetailsViewModel",
+                    "Got habit details with id=$habitId: $habitId"
+                )
+                _state.update {
+                    HabitDetailsState.Content(
+                        habit = habitWithCompletions.habit,
+                        completions = habitWithCompletions.completions,
+                        currentMonth = month
+                    )
+                }
+            }.onError { error ->
+                Log.d(
+                    "HabitDetailsViewModel",
+                    "Got error while getting habit details with id=$habitId: $error"
+                )
+                _state.update {
+                    HabitDetailsState.Initial
+                }
+            }
+        }
+    }
+}
+
+sealed interface HabitDetailsCommand {
+    data class ToggleHabitCompletionOnDate(val selectedDate: LocalDate) : HabitDetailsCommand
+    data class SetDisplayedMonth(val selectedMonth: YearMonth) : HabitDetailsCommand
+    data object Back : HabitDetailsCommand
+}
+
+sealed interface HabitDetailsState {
+    data object Initial : HabitDetailsState
+
+    data class Content(
+        val habit: Habit,
+        val completions: List<HabitCompletion>,
+        val currentMonth: YearMonth
+    ) : HabitDetailsState
+
+    data object Finished : HabitDetailsState
 }
