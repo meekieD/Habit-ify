bf7572a90daacc46b7e6aa2707008bba1774907b
Михаил Дюсов <143973@gmail.com>
2026-03-11 21:50:47 +0300

    core(domain): delete GetHabitWithCompletionsInPeriodUseCase and refactor GetHabitWithCompletionsUseCase

--- core/domain/src/main/java/com/dyusov/core/domain/tracking/GetHabitWithCompletionsUseCase.kt
+++ core/domain/src/main/java/com/dyusov/core/domain/tracking/GetHabitWithCompletionsUseCase.kt
@@ -4,10 +4,11 @@ import com.dyusov.core.common.utils.MyError
 import com.dyusov.core.common.utils.MyResult
 import com.dyusov.core.data.repo.HabitRepository
 import com.dyusov.core.model.HabitWithCompletions
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetHabitWithCompletionsUseCase @Inject constructor(
     private val habitRepository: HabitRepository
 ) {
-    suspend operator fun invoke(habitId: Long): MyResult<HabitWithCompletions, MyError> = habitRepository.getHabitWithCompletions(habitId)
+    suspend operator fun invoke(habitId: Long): Flow<MyResult<HabitWithCompletions, MyError>> = habitRepository.getHabitWithCompletions(habitId)
 }
