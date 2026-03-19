1ae2649e7a8066cad0034bc7c428f20f7b025f9b
Михаил Дюсов <143973@gmail.com>
2026-03-19 21:07:41 +0300

    app: add theme support

--- app/src/main/java/com/dyusov/habit_ify/MainActivity.kt
+++ app/src/main/java/com/dyusov/habit_ify/MainActivity.kt
@@ -5,13 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.dyusov.core.designsystem.ThemeViewModel
 import com.dyusov.habit_ify.presentation.navigation.NavRoute
 import com.dyusov.habit_ify.presentation.theme.ui.HabitifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val themeViewModel: ThemeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // make the app drawing area fullscreen (draw behind status and nav bars)
@@ -27,7 +34,8 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            HabitifyTheme {
+            val themeMode by themeViewModel.themeMode.collectAsStateWithLifecycle()
+            HabitifyTheme(themeMode = themeMode) {
                 NavRoute()
             }
         }
--- app/src/main/java/com/dyusov/habit_ify/presentation/theme/ui/Theme.kt
+++ app/src/main/java/com/dyusov/habit_ify/presentation/theme/ui/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.dyusov.core.designsystem.ThemeMode
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -27,10 +28,16 @@ private val DarkColorScheme = darkColorScheme(
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun HabitifyTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    themeMode: ThemeMode = ThemeMode.SYSTEM,
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val darkTheme = when (themeMode) {
+        ThemeMode.LIGHT -> false
+        ThemeMode.DARK -> true
+        ThemeMode.SYSTEM -> isSystemInDarkTheme()
+    }
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
