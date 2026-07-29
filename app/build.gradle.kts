646681a0fe639680d34a81bddf46bd2f245c4faa
Михаил Дюсов <143973@gmail.com>
2026-07-29 17:48:15 +0300

    app: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.dyusov.habit_ify"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.0"
+        versionCode = 5
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
